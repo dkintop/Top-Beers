4d9ce2363c2bf69d6cd33d5d91e3c274afe42052
Doug Kintop <dkintop@hotmail.com>
2019-09-30 15:52:47 +0000

    setup initial framework and starting point

--- lib/Scraper.rb
+++ lib/Scraper.rb
@@ -1,5 +1,14 @@
 require 'nokogiri'
 require 'pry'
+require 'open-uri'
 
 #scrape beer advocate.com top 250 list
-#get each beer listed into an array 
+#get each beer instance listed into an array
+
+class Scraper 
+  #get array of hashes. each hash relates to one beer on the top 250 list with hash keys to include  name, brewery, type, avg rating, number of ratings
+  def self.scrape_250_list
+    
+  end 
+  
+end 
--- lib/interface.rb
+++ lib/interface.rb
@@ -1,1 +1,11 @@
+require_relative 'Beers.rb'
+require_relative 'Scraper.rb'
+require 'nokogiri'
+
 #sets class used for initializing the interface. refer to student scraper lesson when needed.
+
+class Interface 
+  
+  
+  
+end 
