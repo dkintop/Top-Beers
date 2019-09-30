d0c0f67d688bae74d89e910186fb5ea3c83c76ab
Doug Kintop <dkintop@hotmail.com>
2019-09-30 18:58:42 +0000

    all attributes aquired from nokogiri

--- lib/Scraper.rb
+++ lib/Scraper.rb
@@ -9,19 +9,22 @@ class Scraper
   #get array of hashes. each hash relates to one beer on the top 250 list with hash keys to include  name, brewery, type, 
 
   
-  #def self.ba_scrape
+  def self.ba_scrape
     doc = Nokogiri::HTML(open('https://www.beeradvocate.com/beer/top-rated/'))
     all_beers_xml_array = doc.search(".hr_bottom_light[@align='left']")       #returns array of xml elemts, each correlating with a specific beer.
-    
+    beer_list = []
     
     all_beers_xml_array.each do|beer| 
+      attr_hash = {}
       beer_name = beer.css('a b').text
       brewery = beer.css('span.muted a').first.text
       type = beer.css('span.muted a')[1].text
+      #next step is to place each attribute as a key/value of a hash and put that hash into an array.
+      
       binding.pry
     end
     
- # end 
+  end 
    
   
    
