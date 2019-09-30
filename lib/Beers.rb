262e3d190e45d3102912b463d1491a4341c8dbe5
Doug Kintop <dkintop@hotmail.com>
2019-09-30 18:51:50 +0000

    successfuly obtained css selectors for name, brewery, and type

--- lib/Beers.rb
+++ lib/Beers.rb
@@ -1,8 +1,10 @@
 require "Beer_CLI/version"
 require_relative "Scraper.rb"
 require 'pry'
-#define class Beer, initialize each beer with attributes name, brewery, type, avg rating, number of ratings.
+
 
 class Beer 
+  attr_accessor :name, :brewery, :type
+  
   
 end 
--- lib/Scraper.rb
+++ lib/Scraper.rb
@@ -6,22 +6,31 @@ require 'open-uri'
 
 
 class Scraper 
-  #get array of hashes. each hash relates to one beer on the top 250 list with hash keys to include  name, brewery, type, avg rating, number of ratings
+  #get array of hashes. each hash relates to one beer on the top 250 list with hash keys to include  name, brewery, type, 
+
   
-  
-  def self.top_250_list
+  #def self.ba_scrape
     doc = Nokogiri::HTML(open('https://www.beeradvocate.com/beer/top-rated/'))
-    all_beers_xml_array = doc.search(".hr_bottom_light[@align='left']")
-    all_beers_xml_array
-    binding.pry
-  end 
+    all_beers_xml_array = doc.search(".hr_bottom_light[@align='left']")       #returns array of xml elemts, each correlating with a specific beer.
+    
+    
+    all_beers_xml_array.each do|beer| 
+      beer_name = beer.css('a b').text
+      brewery = beer.css('span.muted a').first.text
+      type = beer.css('span.muted a')[1].text
+      binding.pry
+    end
+    
+ # end 
+   
   
+   
+   
 end 
 
 
 
-    
-    
+   
     #all beers XML array = doc.search(".hr_bottom_light[@align='left']")
     #entire table = doc.css("#ba-content > table")
     #binding.pry
