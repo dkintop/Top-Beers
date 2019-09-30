1080fa196969d9119b9a55db3a430351a43e5e0d
Doug Kintop <dkintop@hotmail.com>
2019-09-30 21:18:21 +0000

    trying to fix all_beers_xml_array

--- lib/Scraper.rb
+++ lib/Scraper.rb
@@ -9,22 +9,22 @@ class Scraper
   #get array of hashes. each hash relates to one beer on the top 250 list with hash keys to include  name, brewery, type, 
 
   
-  #def self.ba_scrape
+  #def self.scrape
     doc = Nokogiri::HTML(open('https://www.beeradvocate.com/beer/top-rated/'))
-    all_beers_xml_array = doc.search(".hr_bottom_light[@align='left']")       #returns array of xml elemts, each correlating with a specific beer.
+    all_beers_xml_array = doc.search(".hr_bottom_light[@align='left'] span.muted") 
     beer_list = []
-    
+    binding.pry
     all_beers_xml_array.each do|beer| 
       beer_hash = {}
-      name = beer.css('a b').text
-      brewery = beer.css('span.muted a').first.text
-      type = beer.css('span.muted a')[1].text
-      abv = beer.css('span').text.split("|")[1].strip
-      #next step is to place each attribute as a key/value of a hash and put that hash into an array.
+      beer_hash[:name] = beer.css('a b').text
+      beer_hash[:brewery] = beer.css('span.muted a').first.text
+      beer_hash[:type] = beer.css('span.muted a')[1].text
+      beer_hash[:abv] = beer.css('span').text.split("|")[1].strip
+      beer_list << beer_hash
       
       binding.pry
     end
-    
+    beer_list
  # end 
    
   
@@ -38,4 +38,10 @@ end
     #all beers XML array = doc.search(".hr_bottom_light[@align='left']")
     #entire table = doc.css("#ba-content > table")
     #binding.pry
+
+
+
+
+   
+  
     
