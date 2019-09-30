c8e10f9e8618b340800d3d94c306148bb0bda825
Doug Kintop <dkintop@hotmail.com>
2019-09-30 17:27:40 +0000

    achieved returning array of xml instances with each element relating to one beer.

--- lib/Scraper.rb
+++ lib/Scraper.rb
@@ -2,32 +2,27 @@ require 'nokogiri'
 require 'pry'
 require 'open-uri'
 
-#scrape beer advocate.com top 250 list
-#get each beer instance listed into an array
+
+
 
 class Scraper 
   #get array of hashes. each hash relates to one beer on the top 250 list with hash keys to include  name, brewery, type, avg rating, number of ratings
   
   
-  def self.scrape_250_list
+  def self.top_250_list
     doc = Nokogiri::HTML(open('https://www.beeradvocate.com/beer/top-rated/'))
-    beers = []
-    
-    
-      
-    
-    
+    all_beers_xml_array = doc.search(".hr_bottom_light[@align='left']")
+    all_beers_xml_array
+    binding.pry
   end 
   
 end 
 
 
-doc = Nokogiri::HTML(open('https://www.beeradvocate.com/beer/top-rated/'))
-doc.css("").text
-    binding.pry
+
     
     
-    
+    #all beers XML array = doc.search(".hr_bottom_light[@align='left']")
     #entire table = doc.css("#ba-content > table")
     #binding.pry
     
