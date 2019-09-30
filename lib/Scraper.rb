ee6e4f1c00d7a87b1daf03920a2da36d6b744e26
Doug Kintop <dkintop@hotmail.com>
2019-09-30 22:03:48 +0000

    continued issues with all_beers array

--- lib/Scraper.rb
+++ lib/Scraper.rb
@@ -9,23 +9,24 @@ class Scraper
   #get array of hashes. each hash relates to one beer on the top 250 list with hash keys to include  name, brewery, type, 
 
   
-  #def self.scrape
+ # def self.scrape
     doc = Nokogiri::HTML(open('https://www.beeradvocate.com/beer/top-rated/'))
-    all_beers = doc.search(".hr_bottom_light[@align='left'] span.muted") 
+    all_beers = doc.search(".hr_bottom_light[@align='left']" #span.muted)  #I only want every 4th element and the first element in this.
     beer_list = []
-    binding.pry
+    beer_hash = {}
+    #binding.pry
     all_beers.each do|beer| 
-      beer_hash = {}
+      
       beer_hash[:name] = beer.css('a b').text
       beer_hash[:brewery] = beer.css('span.muted a').first.text
       beer_hash[:type] = beer.css('span.muted a')[1].text
       beer_hash[:abv] = beer.css('span').text.split("|")[1].strip
       beer_list << beer_hash
       
-      #binding.pry
+      binding.pry
     end
     beer_list
- # end 
+  #end 
    
   
    
