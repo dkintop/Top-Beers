2a00bb2f1480d89e8ef470b2d867b76f39d7e609
Doug Kintop <dkintop@hotmail.com>
2019-10-01 14:13:07 +0000

    corrected scraper method to return hash of top 50 beers

--- lib/Scraper.rb
+++ lib/Scraper.rb
@@ -6,34 +6,29 @@ require 'open-uri'
 
 
 class Scraper 
-  #get array of hashes. each hash relates to one beer on the top 250 list with hash keys to include  name, brewery, type, and abv 
-
   
- # def self.scrape
+  
+  def self.scrape
     doc = Nokogiri::HTML(open('https://www.beeradvocate.com/beer/top-rated/'))
-    all_beers = doc.search(".hr_bottom_light[@align='left']" #span.muted)  #I only want every 4th element and the first element in this.
-    
-    #adding the span.muted selector gives me everything I want except the beer name.
-    
-    
+    all_beers = doc.search(".hr_bottom_light[@align='left']") 
+    i = 1
     beer_list = []
-    beer_hash = {}
-    #binding.pry
     all_beers.each do|beer| 
-      
-      beer_hash[:name] = beer.css('a b').text
-      beer_hash[:brewery] = beer.css('span.muted a').first.text
-      beer_hash[:type] = beer.css('span.muted a')[1].text
-      beer_hash[:abv] = beer.css('span').text.split("|")[1].strip
-      beer_list << beer_hash
-      
-      binding.pry
+      if beer.css('a b').text != "" && i <=50
+        beer_hash = {}
+        beer_hash[:name] = beer.css('a b').text
+        beer_hash[:brewery] = beer.css('span.muted a').first.text
+        beer_hash[:type] = beer.css('span.muted a')[1].text
+        beer_hash[:abv] = beer.css('span').text.split("|")[1].strip
+        beer_list << beer_hash
+        i += 1
+      end
     end
     beer_list
-  #end 
+  end 
    
-  
-   
+  Scraper.scrape
+   binding.pry
    
 end 
 
@@ -43,6 +38,7 @@ end
     #all beers XML array = doc.search(".hr_bottom_light[@align='left']")
     #entire table = doc.css("#ba-content > table")
     #binding.pry
+
 
 
 
